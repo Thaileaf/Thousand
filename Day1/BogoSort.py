106ff73a249e2a0910d2f617d763b6d679c91592
Thaileaf <lethaison2019@gmail.com>
2021-01-01 22:07:25 -0800

    First Commit

---
+++ Day1/BogoSort.py
@@ -0,0 +1,25 @@
+'''
+Day 1 of coding
+Bogo/Monkey sort
+'''
+import random
+
+# class Bogosort:
+#     Completely random version
+#     @staticmethod
+def bogosort(array):
+    permutations = 0
+    while not is_sorted(array):
+        permutations += 1
+        a = random.randint(0, len(array) - 1)
+        b = random.randint(0, len(array) - 1)
+        array[a], array[b] = array[b], array[a]
+        print(array)
+    print(f'It took {permutations} permutations')
+
+def is_sorted(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i+1]:
+            return False
+    else:
+        return True
---
+++ Day1/main.py
@@ -0,0 +1,26 @@
+import BogoSort
+import random
+import time
+
+# calculates average time for bogosort on size n arrays, from low to high n
+def avg_bogosort(low, high):
+    for i in range(low, high + 1):
+        avg_times = []
+        for j in range(0, 10):
+            unsorted = [random.randint(0, i - 1) for x in range(0, i)]
+            start = time.time()
+            BogoSort.bogosort(unsorted)
+            end = time.time()
+            total = end - start
+            avg_times.append(total)
+
+        mean = 0
+        for j in avg_times:
+            mean += j
+        mean / len(avg_times)
+        print(f'It took  average {mean * 1000} milliseconds to bogosort an array with a size of {i}')
+        time.sleep(3)
+
+
+if __name__ == '__main__':
+    avg_bogosort(5, 10)
