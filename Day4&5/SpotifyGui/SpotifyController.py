ef8548dd6d34a09199d0596e1fc5cb665e9d73fc
Thaileaf <lethaison2019@gmail.com>
2021-01-13 22:27:08 -0800

    buttons for saving song and restoring song complete

--- Day4&5/SpotifyGui/SpotifyController.py
+++ Day4&5/SpotifyGui/SpotifyController.py
@@ -1,1 +1,13 @@
-class SpotifyMainMenu:
+from SpotifyView import SpotifyMainView
+class SpotifyCtrl:
+    def __init__(self, model, view):
+        self.get_data = model
+        self.view = view
+
+        self.connect_signals()
+
+
+    def connect_signals(self):
+        self.view.save.clicked.connect(pass) #filler code here
+        self.view.restore.clicked.connect(pass) #filler code here
+
--- Day4&5/SpotifyGui/SpotifyView.py
+++ Day4&5/SpotifyGui/SpotifyView.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QDialogButtonBox
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QFormLayout
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QPushButton
 
 from PyQt5.QtCore import Qt
 
@@ -21,17 +23,48 @@ class SpotifyMainView(QMainWindow):
         self._centralWidget.setLayout(self.generalLayout)
 
         self._createDisplay()
+        self.login = LoginDialog()
+        self.login.exec()
 
     def _createDisplay(self):
+        self.save = QPushButton('Save songs')
+        self.restore = QPushButton('Restore songs')
+
+        self.generalLayout.addWidget(self.save)
+        self.generalLayout.addWidget(self.restore)
+
+class LoginDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(LoginDialog, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle('Login')
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        print(buttons)
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
         self.user = QLineEdit()
         self.passW = QLineEdit()
-        self.ulabel = QLabel
+        self.ulabel = QLabel()
+        self.ulabel.setText('Username')
+        self.plabel = QLabel()
+        self.plabel.setText('Password')
 
         self.user.setFixedHeight(35)
         self.passW.setFixedHeight(35)
 
-        self.generalLayout.addWidget(self.user)
-        self.generalLayout.addWidget(self.passW)
+
+
+        self.layout = QFormLayout()
+        self.layout.addWidget(self.ulabel)
+        self.layout.addWidget(self.user)
+        self.layout.addWidget(self.plabel)
+        self.layout.addWidget(self.passW)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 
 
